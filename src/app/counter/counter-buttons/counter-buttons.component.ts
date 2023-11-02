56f856e18d167463cb13cb2a6e938b7ce9608c13
Oleksii Ivasov <Oleksii_Ivasov@epam.com>
2023-11-02 13:54:40 +0200

    initial commit

---
+++ src/app/counter/counter-buttons/counter-buttons.component.ts
@@ -0,0 +1,12 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-counter-buttons',
+  templateUrl: './counter-buttons.component.html',
+  styleUrls: ['./counter-buttons.component.scss']
+})
+export class CounterButtonsComponent {
+onIncrement() {
+  
+}
+}
